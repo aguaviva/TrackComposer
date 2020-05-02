ba6575b3aa27140f846981aa489c5040a3eb89a2
Aguaviva, Raul <raul.aguaviva@amd.com>
2020-05-02 17:14:01 +0200

    KarplusStrong is working (I am quite impressed!)

--- app/src/main/java/com/example/trackcomposer/GeneratorKarplusStrong.java
+++ app/src/main/java/com/example/trackcomposer/GeneratorKarplusStrong.java
@@ -9,52 +9,48 @@ public class GeneratorKarplusStrong extends Generator {
     GeneratorKarplusStrong()
     {
         SetSampleRate(44100);
-        delayLine = new float[1024];
+        delayLine = new float[44100]; //TODO: compute a better size
         instrumentName = "GeneratorKarplusStrong";
     }
 
     void pluck(int length)
     {
-        length = Math.max(length, delayLine.length);
+        length = Math.min(length, delayLine.length);
 
         for(int i=0;i<length;i++)
         {
-            delayLine[i] = (float)(2.0f*Math.random()-0.5f);
+            delayLine[i] = (float)(Math.random()-0.5f);
         }
 
         startDelay = 0;
-        endDelay = length;
+        endDelay = length-1;
     }
 
     float SynthStep()
     {
         float v1 = delayLine[startDelay];
-
         startDelay++;
-        endDelay++;
-
         if (startDelay>=delayLine.length)
             startDelay = 0;
-
-        if (endDelay>=delayLine.length)
-            endDelay = 0;
 
         float v2 = delayLine[startDelay];
 
         delayLine[endDelay] = (v1+v2) * 0.5f * 0.994f;
+        endDelay++;
+        if (endDelay>=delayLine.length)
+            endDelay = 0;
 
         return v1;
     }
-    
+
     @Override
     public void playSample(Mixer.Channel channel, short[] chunk, int ini, int fin) {
 
         if (channel.timeInSamples==0)
         {
-             pluck((int)(channel.speed*delayLine.length/4.0f));
+             pluck((int)(44100.0f / (channel.speed*220.0f)));
         }
 
-        float t = channel.timeInSamples * channel.speed;
         float timeInSeconds = channel.timeInSamples * GetInvSampleRate();
 
         for (int i = ini; i < fin; i += 2) {
@@ -64,8 +60,13 @@ public class GeneratorKarplusStrong extends Generator {
             chunk[i + 0] += v;
             chunk[i + 1] += v;
 
-            t += channel.speed;
             channel.timeInSamples++;
+
+            if (channel.timeInSamples>44100.0f/1.0f) {
+                channel.mPlaying = false;
+                break;
+            }
+
             timeInSeconds += GetInvSampleRate();
         }
     }
